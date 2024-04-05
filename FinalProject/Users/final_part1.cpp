c8564a882ba3a4b1003073d58c72765621401036
LGugs3 <liam.gugliotta@mymail.champlain.edu>
2024-04-05 13:58:53 -0400

    Created and Finished testing Account class by itself

---
+++ FinalProject/Users/final_part1.cpp
@@ -0,0 +1,27 @@
+/*
+* Author: Liam Gugliotta
+Class: CSI-240-04
+Assignment: Final Project Part1
+Date Assigned: 04/05/2024
+Due Date: 04/09/2024
+Description:
+The Main file for part 1 of the final project.
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition and
+consequences of plagiarism and acknowledge that the assessor of this assignment
+may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic staff;
+and/or
+- Communicate a copy of this assignment to a plagiarism checking service (which may
+then retain a copy of this assignment on its database for the purpose of future
+plagiarism checking)
+*/
+
+#include "account.h"
+
+
+int main()
+{
+	
+}
